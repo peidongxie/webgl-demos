db24366f5cf809e32a45ed69cc13753acb938b42
谢沛东 <peidongxie@outlook.com>
2023-07-12 12:34:56 +0800

    feat: 实现矩阵乘法

--- src/lib/cuon-matrix.ts
+++ src/lib/cuon-matrix.ts
@@ -51,6 +51,68 @@ class Matrix4 {
     }
     return this;
   }
+
+  concat(other: Matrix4): this {
+    const a = this.elements;
+    const b = a === other.elements ? a.slice() : other.elements;
+    const c = this.elements;
+    for (let i = 0; i < 4; i++) {
+      const ai0 = a[i];
+      const ai1 = a[i + 4];
+      const ai2 = a[i + 8];
+      const ai3 = a[i + 12];
+      for (let j = 0; j < 4; j++) {
+        const b0j = b[j * 4];
+        const b1j = b[j * 4 + 1];
+        const b2j = b[j * 4 + 2];
+        const b3j = b[j * 4 + 3];
+        c[i + j * 4] = ai0 * b0j + ai1 * b1j + ai2 * b2j + ai3 * b3j;
+      }
+    }
+    return this;
+  }
+
+  multiply(other: Matrix4): this {
+    return this.concat(other);
+  }
+
+  multiplyVector3(pos: Vector3): Vector3 {
+    const a = this.elements;
+    const b = pos.elements;
+    const result = new Vector3();
+    const c = result.elements;
+    for (let i = 0; i < 3; i++) {
+      const ai0 = a[i];
+      const ai1 = a[i + 4];
+      const ai2 = a[i + 8];
+      const ai3 = a[i + 12];
+      const b00 = b[0];
+      const b10 = b[1];
+      const b20 = b[2];
+      const b30 = 1;
+      c[i] = ai0 * b00 + ai1 * b10 + ai2 * b20 + ai3 * b30;
+    }
+    return result;
+  }
+
+  multiplyVector4(pos: Vector4): Vector4 {
+    const a = this.elements;
+    const b = pos.elements;
+    const result = new Vector4();
+    const c = result.elements;
+    for (let i = 0; i < 4; i++) {
+      const ai0 = a[i];
+      const ai1 = a[i + 4];
+      const ai2 = a[i + 8];
+      const ai3 = a[i + 12];
+      const b00 = b[0];
+      const b10 = b[1];
+      const b20 = b[2];
+      const b30 = b[3];
+      c[i] = ai0 * b00 + ai1 * b10 + ai2 * b20 + ai3 * b30;
+    }
+    return result;
+  }
 }
 
 export { Matrix4, Vector3, Vector4 };
