c495601ecf82e01cdaf22e08f8dd6774d9f423ec
谢沛东 <peidongxie@outlook.com>
2023-07-26 12:34:56 +0800

    feat: 实现 demo-04

--- src/webgl/index.tsx
+++ src/webgl/index.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Demo01 from './demo-01';
 import Demo02 from './demo-02';
 import Demo03 from './demo-03';
+import Demo04 from './demo-04';
 
 interface WebglProps {
   [key: string]: never;
@@ -15,6 +16,7 @@ const Webgl: FC<WebglProps> = () => {
       <Route path={'/demo-01'} element={<Demo01 />} />
       <Route path={'/demo-02'} element={<Demo02 />} />
       <Route path={'/demo-03'} element={<Demo03 />} />
+      <Route path={'/demo-04'} element={<Demo04 />} />
     </Routes>
   );
 };
