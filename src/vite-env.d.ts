e1accbb52cd91606772489b52029f71c35ea5061
谢沛东 <peidongxie@outlook.com>
2023-09-05 12:34:56 +0800

    fix: 修正 404 页面资源路径

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,5 @@
 /// <reference types='vite/client' />
 
-interface ImportMetaEnv {
-  readonly VITE_BASENAME: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
 declare interface HTMLCanvasElement {
   addEventListener(
     type: 'webglcontextcreationerror',
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.PUBLIC_URL || '/',
+    plugins: [react()],
+  };
 });
