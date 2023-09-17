c2e6deebd602f0a3d83ad7aa57dae1869a6592a7
谢沛东 <peidongxie@outlook.com>
2023-09-17 12:34:56 +0800

    feat: 优化上下文逻辑

--- src/webgl/demo-14/index.tsx
+++ src/webgl/demo-14/index.tsx
@@ -24,51 +24,58 @@ const Demo14: FC<ComponentProps> = () => {
   const [[translationX, translationY, translationZ]] = useState([0.5, 0.5, 0]);
 
   useEffect(() => {
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      canvas.width = canvas.clientWidth;
-      canvas.height = canvas.clientHeight;
-    }
-  }, []);
-
-  useEffect(() => {
-    if (canvasRef.current && !glRef.current) {
-      const gl = getWebGLContext(canvasRef.current);
-      if (gl && initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-        /**
-         * 变量位置
-         */
-        const positionAttributeLocation = gl.getAttribLocation(
-          gl.program,
-          'a_Position',
-        );
-        const translationUniformLocation = gl.getUniformLocation(
-          gl.program,
-          'u_Translation',
-        );
-        positionAttributeLocationRef.current = positionAttributeLocation;
-        translationUniformLocationRef.current = translationUniformLocation;
-        /**
-         * 缓冲区
-         */
-        const vertexBuffer = gl.createBuffer();
-        vertexBufferRef.current = vertexBuffer;
-        /**
-         * 清空
-         */
-        gl.clearColor(0, 0, 0, 1);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-      }
-      glRef.current = gl;
-    }
-    return () => {
-      glRef.current = null;
-    };
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
   }, []);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
+    const gl = glRef.current;
+    if (gl) return;
+    glRef.current = getWebGLContext(canvasRef.current);
+  }, []);
+
+  useEffect(
+    () => () => {
+      glRef.current = null;
+    },
+    [],
+  );
+
+  useEffect(() => {
+    const gl = glRef.current;
+    if (!gl) return;
+    const success = initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+    if (success) {
+      /**
+       * 变量位置
+       */
+      const positionAttributeLocation = gl.getAttribLocation(
+        gl.program,
+        'a_Position',
+      );
+      const translationUniformLocation = gl.getUniformLocation(
+        gl.program,
+        'u_Translation',
+      );
+      positionAttributeLocationRef.current = positionAttributeLocation;
+      translationUniformLocationRef.current = translationUniformLocation;
+      /**
+       * 缓冲区
+       */
+      const vertexBuffer = gl.createBuffer();
+      vertexBufferRef.current = vertexBuffer;
+      /**
+       * 清空设置
+       */
+      gl.clearColor(0, 0, 0, 1);
+    }
+  }, []);
+
+  useEffect(() => {
     const gl = glRef.current;
     if (!gl) return;
     const positionAttributeLocation = positionAttributeLocationRef.current;
@@ -77,6 +84,9 @@ const Demo14: FC<ComponentProps> = () => {
     if (!translationUniformLocation) return;
     const vertexBuffer = vertexBufferRef.current;
     if (!vertexBuffer) return;
+    /**
+     * 清空
+     */
     gl.clear(gl.COLOR_BUFFER_BIT);
     /**
      * 数据写入缓冲区并分配到变量，绘制
