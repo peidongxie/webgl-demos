10042fe673d4d3481fa50d1a94885e27eec0db57
谢沛东 <peidongxie@outlook.com>
2023-11-18 00:36:23 +0800

    feat: 实现 demo-45

--- src/router.tsx
+++ src/router.tsx
@@ -55,6 +55,7 @@ const WebglDemo41 = lazy(() => import('./webgl/demo-41'));
 const WebglDemo42 = lazy(() => import('./webgl/demo-42'));
 const WebglDemo43 = lazy(() => import('./webgl/demo-43'));
 const WebglDemo44 = lazy(() => import('./webgl/demo-44'));
+const WebglDemo45 = lazy(() => import('./webgl/demo-45'));
 const MarsDemo01 = lazy(() => import('./mars/demo-01'));
 const GalaceanDemo01 = lazy(() => import('./galacean/demo-01'));
 const GalaceanDemo02 = lazy(() => import('./galacean/demo-02'));
@@ -414,6 +415,14 @@ const webglChildren: RouteObject[] = [
     element: <WebglDemo44 />,
     loader: (): MatchData => ({
       value: ['44 消除隐藏面', '/demo-44'],
+      children: [],
+    }),
+  },
+  {
+    path: 'demo-45',
+    element: <WebglDemo45 />,
+    loader: (): MatchData => ({
+      value: ['45 消除隐藏面', '/demo-45'],
       children: [],
     }),
   },
---
+++ src/webgl/demo-45/index.tsx
@@ -0,0 +1,198 @@
+import { type FC, useEffect, useMemo, useRef, useState } from 'react';
+
+import { type ComponentProps } from '../../type';
+import { Matrix4 } from '../lib/cuon-matrix';
+import { getWebGLContext, initShaders } from '../lib/cuon-utils';
+import { useFloat32Array } from '../lib/react-utils';
+import FSHADER_SOURCE from './fragment.glsl?raw';
+import VSHADER_SOURCE from './vertex.glsl?raw';
+
+/**
+ * 解决深度冲突
+ */
+const Demo45: FC<ComponentProps> = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const glRef = useRef<WebGLRenderingContext | null>(null);
+  const positionAttributeRef = useRef(-1);
+  const colorAttributeRef = useRef(-1);
+  const viewProjMatrixUniformRef = useRef<WebGLUniformLocation | null>(null);
+  const positionColorBufferRef = useRef<WebGLBuffer | null>(null);
+  const [points] = useState<
+    [number, number, number, number, number, number][][]
+  >([
+    [
+      [0.0, 2.5, -5.0, 0.4, 1.0, 0.4],
+      [-2.5, -2.5, -5.0, 0.4, 1.0, 0.4],
+      [2.5, -2.5, -5.0, 1.0, 0.4, 0.4],
+    ],
+    [
+      [0.0, 3.0, -5.0, 1.0, 0.4, 0.4],
+      [-3.0, -3.0, -5.0, 1.0, 1.0, 0.4],
+      [3.0, -3.0, -5.0, 1.0, 1.0, 0.4],
+    ],
+  ]);
+  const positionsColors = useFloat32Array(points);
+  const [[eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ]] =
+    useState<
+      [number, number, number, number, number, number, number, number, number]
+    >([3.06, 2.5, 10, 0, 0, -2, 0, 1, 0]);
+  const viewMatrix = useMemo(() => {
+    const viewMatrix = new Matrix4();
+    viewMatrix.setLookAt(
+      eyeX,
+      eyeY,
+      eyeZ,
+      centerX,
+      centerY,
+      centerZ,
+      upX,
+      upY,
+      upZ,
+    );
+    return viewMatrix;
+  }, [eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ]);
+  const [[fovy, aspect, near, far], setPerspective] = useState<
+    [number, number, number, number]
+  >([30, 1, 1, 100]);
+  const projMatrix = useMemo(() => {
+    const projMatrix = new Matrix4();
+    projMatrix.setPerspective(fovy, aspect, near, far);
+    return projMatrix;
+  }, [fovy, aspect, near, far]);
+  const viewProjMatrix = useMemo(() => {
+    return new Matrix4(projMatrix).multiply(viewMatrix);
+  }, [viewMatrix, projMatrix]);
+  const [deps, setDeps] = useState<[Float32Array | null, Matrix4 | null]>([
+    null,
+    null,
+  ]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    setPerspective((perspective) => [
+      perspective[0],
+      canvas.width / canvas.height,
+      perspective[2],
+      perspective[3],
+    ]);
+  }, []);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const gl = glRef.current;
+    if (gl) return;
+    glRef.current = getWebGLContext(canvasRef.current);
+  }, []);
+
+  useEffect(() => {
+    const gl = glRef.current;
+    if (!gl) return;
+    const success = initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+    if (!success) return;
+    /**
+     * 变量位置
+     */
+    const positionAttribute = gl.getAttribLocation(gl.program, 'a_Position');
+    const colorAttribute = gl.getAttribLocation(gl.program, 'a_Color');
+    const viewProjMatrixUniform = gl.getUniformLocation(
+      gl.program,
+      'u_ViewProjMatrix',
+    );
+    positionAttributeRef.current = positionAttribute;
+    colorAttributeRef.current = colorAttribute;
+    viewProjMatrixUniformRef.current = viewProjMatrixUniform;
+    /**
+     * 缓冲区
+     */
+    const positionColorBuffer = gl.createBuffer();
+    positionColorBufferRef.current = positionColorBuffer;
+    /**
+     * 清空、深度和偏移设置
+     */
+    gl.clearColor(0, 0, 0, 1);
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.POLYGON_OFFSET_FILL);
+  }, []);
+
+  useEffect(() => {
+    const gl = glRef.current;
+    if (!gl) return;
+    const positionAttribute = positionAttributeRef.current;
+    if (positionAttribute < 0) return;
+    const colorAttribute = colorAttributeRef.current;
+    if (colorAttribute < 0) return;
+    const positionColorBuffer = positionColorBufferRef.current;
+    if (!positionColorBuffer) return;
+    /**
+     * 数据写入缓冲区并分配到变量
+     */
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, positionsColors, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(
+      positionAttribute,
+      3,
+      gl.FLOAT,
+      false,
+      positionsColors.BYTES_PER_ELEMENT * 6,
+      0,
+    );
+    gl.enableVertexAttribArray(positionAttribute);
+    gl.vertexAttribPointer(
+      colorAttribute,
+      3,
+      gl.FLOAT,
+      false,
+      positionsColors.BYTES_PER_ELEMENT * 6,
+      positionsColors.BYTES_PER_ELEMENT * 3,
+    );
+    gl.enableVertexAttribArray(colorAttribute);
+    setDeps((deps) => [positionsColors, deps[1]]);
+  }, [positionsColors]);
+
+  useEffect(() => {
+    const gl = glRef.current;
+    if (!gl) return;
+    const viewProjMatrixUniform = viewProjMatrixUniformRef.current;
+    if (!viewProjMatrixUniform) return;
+    /**
+     * 数据直接分配到变量
+     */
+    gl.uniformMatrix4fv(viewProjMatrixUniform, false, viewProjMatrix.elements);
+    setDeps((deps) => [deps[0], viewProjMatrix]);
+  }, [viewProjMatrix]);
+
+  useEffect(() => {
+    const gl = glRef.current;
+    if (!gl) return;
+    const viewProjMatrixUniform = viewProjMatrixUniformRef.current;
+    if (!viewProjMatrixUniform) return;
+    if (deps.some((dep) => dep === null)) return;
+    /**
+     * 清空
+     */
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    /**
+     * 绘制
+     */
+    gl.polygonOffset(0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, Math.floor(deps[0]!.length / 12));
+    gl.polygonOffset(1, 1);
+    gl.drawArrays(
+      gl.TRIANGLES,
+      Math.floor(deps[0]!.length / 12),
+      Math.floor(deps[0]!.length / 12),
+    );
+  }, [deps]);
+
+  return (
+    <canvas ref={canvasRef} style={{ width: '100vw', height: '100vh' }}>
+      {'Please use a browser that supports "canvas"'}
+    </canvas>
+  );
+};
+
+export default Demo45;
