4efd1dffbb7ec45ce9fd00d3f46cb80cbc4c5767
谢沛东 <peidongxie@outlook.com>
2024-01-06 22:01:18 +0800

    feat: 补全兜底

--- src/galacean-engine/index.tsx
+++ src/galacean-engine/index.tsx
@@ -1,10 +1,24 @@
-import { type FC } from 'react';
+import { type FC, Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { type ComponentProps } from '../type';
 
 const Galacean: FC<ComponentProps> = () => {
-  return <Outlet />;
+  return (
+    <Suspense
+      fallback={
+        <div
+          style={{
+            width: '100vw',
+            height: '100vh',
+            backgroundColor: '#ffffff',
+          }}
+        />
+      }
+    >
+      <Outlet />
+    </Suspense>
+  );
 };
 
 export default Galacean;
