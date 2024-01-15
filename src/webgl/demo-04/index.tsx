3866e76984d2c976fcf15dae256defaa6723dd25
谢沛东 <peidongxie@outlook.com>
2024-01-16 01:19:05 +0800

    refactor: 重构有变量示例

--- src/webgl/demo-04/index.tsx
+++ src/webgl/demo-04/index.tsx
@@ -1,59 +1,72 @@
-import { type FC, useEffect, useRef, useState } from 'react';
+import { type FC, useEffect, useRef } from 'react';
 
 import { type ComponentProps } from '../../type';
 import Canvas from '../lib/canvas-component';
 import { initShaders } from '../lib/cuon-utils';
+import {
+  type BaseState,
+  parseStateStore,
+  type StateChangeAction,
+} from '../lib/webgl-store';
 import FSHADER_SOURCE from './fragment.glsl?raw';
 import VSHADER_SOURCE from './vertex.glsl?raw';
+
+interface DemoState extends BaseState {
+  a_Position: number;
+  point: [number, number];
+}
+
+const main = (gl: WebGLRenderingContext): StateChangeAction<DemoState> => {
+  const draw = parseStateStore<DemoState>({
+    // 着色器程序
+    root: {
+      deps: ['a_Position'],
+      data: () => {
+        gl.clearColor(0, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+      },
+      onChange: () => {
+        gl.drawArrays(gl.POINTS, 0, 1);
+      },
+    },
+    // 着色器变量：a_Position
+    a_Position: {
+      deps: ['point'],
+      data: gl.getAttribLocation(gl.program, 'a_Position'),
+      onChange: ({ a_Position, point }) => {
+        const [x, y] = point;
+        gl.vertexAttrib3f(a_Position, x, y, 0);
+      },
+    },
+    // 原子数据：顶点
+    point: {
+      deps: [],
+      data: [0, 0],
+    },
+  });
+  return draw;
+};
 
 /**
  * 动态绘制点
  */
 const Demo04: FC<ComponentProps> = () => {
   const glRef = useRef<WebGLRenderingContext>(null);
-  const positionAttributeRef = useRef(-1);
-  const [point] = useState<[number, number]>([0, 0]);
-  const [deps, setDeps] = useState<[[number, number] | null]>([null]);
+  const drawRef = useRef<StateChangeAction<DemoState> | null>(null);
 
   useEffect(() => {
     const gl = glRef.current;
     if (!gl) return;
     const success = initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
     if (!success) return;
-    /**
-     * 变量位置
-     */
-    const positionAttribute = gl.getAttribLocation(gl.program, 'a_Position');
-    positionAttributeRef.current = positionAttribute;
-    /**
-     * 清空设置
-     */
-    gl.clearColor(0, 0, 0, 1);
+    drawRef.current = main(gl);
   }, []);
 
   useEffect(() => {
-    const gl = glRef.current;
-    if (!gl) return;
-    const positionAttribute = positionAttributeRef.current;
-    if (positionAttribute < 0) return;
-    /**
-     * 数据直接分配到变量
-     */
-    const [x, y] = point;
-    gl.vertexAttrib3f(positionAttribute, x, y, 0);
-    setDeps(() => [point]);
-  }, [point]);
-
-  useEffect(() => {
-    const gl = glRef.current;
-    if (!gl) return;
-    if (deps.some((dep) => dep === null)) return;
-    /**
-     * 清空并绘制
-     */
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, 1);
-  }, [deps]);
+    const draw = drawRef.current;
+    if (!draw) return;
+    draw({ point: [0, 0] });
+  }, []);
 
   return (
     <Canvas
