7f8c206fb74c8affcf1eb2553ee5ea9e6368fee9
谢沛东 <peidongxie@outlook.com>
2024-01-23 02:22:13 +0800

    refactor: 重构变换示例

--- src/webgl/demo-14/index.tsx
+++ src/webgl/demo-14/index.tsx
@@ -1,104 +1,121 @@
-import { type FC, useEffect, useRef, useState } from 'react';
+import { type FC, useEffect, useRef } from 'react';
 
-import { useFloat32Array } from '../../lib/react-utils';
+import { flatArray } from '../../lib/react-utils';
 import { type ComponentProps } from '../../type';
 import Canvas from '../lib/canvas-component';
 import { initShaders } from '../lib/cuon-utils';
+import {
+  type BaseState,
+  parseStateStore,
+  type StateChangeAction,
+} from '../lib/webgl-store';
 import FSHADER_SOURCE from './fragment.glsl?raw';
 import VSHADER_SOURCE from './vertex.glsl?raw';
+
+interface DemoState extends BaseState {
+  a_Position: GLint;
+  u_Translation: WebGLUniformLocation | null;
+  positionBuffer: WebGLBuffer | null;
+  positionArray: Float32Array;
+  points: [number, number][];
+  translation: [number, number, number];
+}
+
+const main = (gl: WebGLRenderingContext): StateChangeAction<DemoState> => {
+  const draw = parseStateStore<DemoState>({
+    // 着色器程序
+    root: {
+      deps: ['a_Position', 'u_Translation'],
+      data: () => {
+        gl.clearColor(0, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+      },
+      onChange: ({ points }) => {
+        gl.drawArrays(gl.TRIANGLES, 0, points.length);
+      },
+    },
+    // 着色器变量：a_Position
+    a_Position: {
+      deps: ['positionBuffer'],
+      data: gl.getAttribLocation(gl.program, 'a_Position'),
+      onChange: ({ a_Position }) => {
+        gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(a_Position);
+      },
+    },
+    // 着色器变量：u_Translation
+    u_Translation: {
+      deps: ['translation'],
+      data: gl.getUniformLocation(gl.program, 'u_Translation'),
+      onChange: ({ u_Translation, translation }) => {
+        const [translationX, translationY, translationZ] = translation;
+        gl.uniform4f(
+          u_Translation,
+          translationX,
+          translationY,
+          translationZ,
+          0,
+        );
+      },
+    },
+    // 派生数据：顶点位置缓冲区
+    positionBuffer: {
+      deps: ['positionArray'],
+      data: gl.createBuffer(),
+      onChange: ({ positionBuffer, positionArray }) => {
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, positionArray, gl.STATIC_DRAW);
+      },
+    },
+    // 派生数据：顶点位置数组
+    positionArray: {
+      deps: ['points'],
+      data: new Float32Array(6),
+      onChange: ({ positionArray, points }) => {
+        positionArray.set(flatArray(points));
+      },
+    },
+    // 原子数据：顶点
+    points: {
+      deps: [],
+      data: [],
+    },
+    // 原子数据：平移
+    translation: {
+      deps: [],
+      data: [0, 0, 0],
+    },
+  });
+  return draw;
+};
 
 /**
  * 向量平移
  */
 const Demo14: FC<ComponentProps> = () => {
   const glRef = useRef<WebGLRenderingContext>(null);
-  const positionAttributeRef = useRef(-1);
-  const translationUniformRef = useRef<WebGLUniformLocation | null>(null);
-  const positionBufferRef = useRef<WebGLBuffer | null>(null);
-  const [points] = useState<[number, number][]>([
-    [0, 0.5],
-    [-0.5, -0.5],
-    [0.5, -0.5],
-  ]);
-  const positions = useFloat32Array(points);
-  const [translation] = useState<[number, number, number]>([0.5, 0.5, 0]);
-  const [deps, setDeps] = useState<
-    [Float32Array | null, [number, number, number] | null]
-  >([null, null]);
+  const drawRef = useRef<StateChangeAction<DemoState> | null>(null);
 
   useEffect(() => {
     const gl = glRef.current;
     if (!gl) return;
     const success = initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
     if (!success) return;
-    /**
-     * 变量位置
-     */
-    const positionAttribute = gl.getAttribLocation(gl.program, 'a_Position');
-    const translationUniform = gl.getUniformLocation(
-      gl.program,
-      'u_Translation',
-    );
-    positionAttributeRef.current = positionAttribute;
-    translationUniformRef.current = translationUniform;
-    /**
-     * 缓冲区
-     */
-    const positionBuffer = gl.createBuffer();
-    positionBufferRef.current = positionBuffer;
-    /**
-     * 清空和变量设置
-     */
-    gl.clearColor(0, 0, 0, 1);
-    positionAttribute >= 0 && gl.enableVertexAttribArray(positionAttribute);
+    drawRef.current = main(gl);
   }, []);
 
   useEffect(() => {
-    const gl = glRef.current;
-    if (!gl) return;
-    const positionAttribute = positionAttributeRef.current;
-    if (positionAttribute < 0) return;
-    const positionBuffer = positionBufferRef.current;
-    if (!positionBuffer) return;
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    /**
-     * 数据写入缓冲区并分配到变量
-     */
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
-    gl.vertexAttribPointer(positionAttribute, 2, gl.FLOAT, false, 0, 0);
-    setDeps((deps) => [positions, deps[1]]);
-  }, [positions]);
-
-  useEffect(() => {
-    const gl = glRef.current;
-    if (!gl) return;
-    const translationUniform = translationUniformRef.current;
-    if (!translationUniform) return;
-    /**
-     * 数据直接分配到变量
-     */
-    const [translationX, translationY, translationZ] = translation;
-    gl.uniform4f(
-      translationUniform,
-      translationX,
-      translationY,
-      translationZ,
-      0,
-    );
-    setDeps((deps) => [deps[0], translation]);
-  }, [translation]);
-
-  useEffect(() => {
-    const gl = glRef.current;
-    if (!gl) return;
-    if (deps.some((dep) => dep === null)) return;
-    /**
-     * 清空并绘制
-     */
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, Math.floor(deps[0]!.length / 2));
-  }, [deps]);
+    const draw = drawRef.current;
+    if (!draw) return;
+    draw({
+      points: [
+        [0, 0.5],
+        [-0.5, -0.5],
+        [0.5, -0.5],
+      ],
+      translation: [0.5, 0.5, 0],
+    });
+  }, []);
 
   return (
     <Canvas
