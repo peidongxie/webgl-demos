9fe2eb471460950e9b0cc545c6d0d68eba779ade
谢沛东 <peidongxie@outlook.com>
2024-02-04 02:58:27 +0800

    feat: 增加画布上下文初始化回调

--- src/webgl/lib/canvas-component.tsx
+++ src/webgl/lib/canvas-component.tsx
@@ -9,26 +9,55 @@ import {
 import { getWebGLContext } from './cuon-utils';
 
 interface CanvasProps extends HTMLAttributes<HTMLCanvasElement> {
-  onWindowResize?: (canvas?: HTMLCanvasElement) => void;
+  // glVertexShader: string;
+  // glFragmentShader: string;
+  onContextInit?: (
+    canvas: HTMLCanvasElement,
+    gl: WebGLRenderingContext,
+  ) => void;
+  onWindowResize?: (
+    canvas?: HTMLCanvasElement,
+    gl?: WebGLRenderingContext,
+  ) => void;
 }
 
 const Canvas = forwardRef<WebGLRenderingContext | null, CanvasProps>(
   (props, ref) => {
-    const { onWindowResize, ...canvasProps } = props;
+    const { onContextInit, onWindowResize, ...canvasProps } = props;
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const glRef = useRef<WebGLRenderingContext | null>(null);
+    const initRef = useRef(() => {});
+    initRef.current = () => {
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+      const gl = glRef.current || getWebGLContext(canvas);
+      if (!gl) return;
+      const program = gl.getParameter(gl.CURRENT_PROGRAM);
+      if (program) return;
+      // const success = initShaders(gl, glVertexShader, glFragmentShader);
+      // if (!success) return;
+      canvas.width = canvas.clientWidth;
+      canvas.height = canvas.clientHeight;
+      gl.viewport(0, 0, canvas.width, canvas.height);
+      glRef.current = gl;
+      onContextInit?.(canvas, gl);
+    };
     const resizerRef = useRef(() => {});
     resizerRef.current = () => {
       const canvas = canvasRef.current;
+      if (!canvas) return onWindowResize?.();
       const gl = glRef.current;
-      const callback = onWindowResize;
-      if (!canvas) return callback?.();
+      if (!gl) return onWindowResize?.(canvas);
       canvas.width = canvas.clientWidth;
       canvas.height = canvas.clientHeight;
-      gl?.viewport(0, 0, canvas.width, canvas.height);
-      return callback?.(canvas);
+      gl.viewport(0, 0, canvas.width, canvas.height);
+      return onWindowResize?.(canvas, gl);
     };
+
+    useLayoutEffect(() => {
+      initRef.current();
+    }, []);
 
     useLayoutEffect(() => {
       let timeoutId = 0;
@@ -38,15 +67,6 @@ const Canvas = forwardRef<WebGLRenderingContext | null, CanvasProps>(
       };
       globalThis.addEventListener('resize', listener);
       return () => globalThis.removeEventListener('resize', listener);
-    }, []);
-
-    useLayoutEffect(() => {
-      const canvas = canvasRef.current;
-      if (!canvas) return;
-      const gl = glRef.current;
-      if (gl) return;
-      glRef.current = getWebGLContext(canvasRef.current);
-      resizerRef.current();
     }, []);
 
     useImperativeHandle<
